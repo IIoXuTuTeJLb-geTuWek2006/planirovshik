da9103633c15c655ec7194923fbdd2144333ceb1
IIoXuTuTeJLb-geTuWek2006 <vasiljev0810@gmail.com>
2025-12-12 16:40:46 +0700

    сохранение задач в json имеется

---
+++ planirovshik_v0.1/ ISaveList.cs
@@ -0,0 +1,8 @@
+﻿namespace planirovshik_v0._1
+{
+    public interface ISaveList<T>
+    {
+        T Load(string path);
+        void Save(T data, string path);
+    }
+}
---
+++ planirovshik_v0.1/JsonTaskSaver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace planirovshik_v0._1
+{
+    public class JsonTaskSaver : ISaveList<List<TaskItem>>
+    {
+        private readonly JsonSerializerOptions _options = new()
+        {
+            WriteIndented = true
+        };
+
+        public List<TaskItem> Load(string path)
+        {
+            if (!File.Exists(path)) return new List<TaskItem>();
+
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<List<TaskItem>>(json, _options)
+                   ?? new List<TaskItem>();
+        }
+
+        public void Save(List<TaskItem> data, string path)
+        {
+            string json = JsonSerializer.Serialize(data, _options);
+            File.WriteAllText(path, json);
+        }
+    }
+}
