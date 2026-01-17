6ad39cd82fd02d6e76245bfad8c9d50280d37d02
IIoXuTuTeJLb-geTuWek2006 <vasiljev0810@gmail.com>
2026-01-17 19:27:31 +0700

    добавил модель задач

---
+++ planirovshik_v0.1/TaskItem.cs
@@ -0,0 +1,26 @@
+﻿namespace planirovshik_v0._1
+{
+    public enum TaskStatus
+    {
+        Planned,
+        InProgress,
+        Done,
+        Postponed
+    }
+
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public class TaskItem
+    {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime DueDate { get; set; } = DateTime.Now;
+        public TaskPriority Priority { get; set; } = TaskPriority.Medium;
+        public TaskStatus Status { get; set; } = TaskStatus.Planned;
+    }
+}
