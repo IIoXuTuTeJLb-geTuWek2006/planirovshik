f0bdeb5b444e66409425b663c90b0e57fa2af19b
IIoXuTuTeJLb-geTuWek2006 <vasiljev0810@gmail.com>
2026-01-17 19:14:55 +0700

    добавил контроллер задач с фильтрацией

---
+++ planirovshik_v0.1/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace planirovshik_v0._1
+{
+    public class TaskController
+    {
+        private readonly List<TaskItem> _tasks = new();
+
+        public IReadOnlyList<TaskItem> Tasks => _tasks;
+
+        public void Add(TaskItem task) => _tasks.Add(task);
+        public void Remove(TaskItem task) => _tasks.Remove(task);
+
+        public IEnumerable<TaskItem> GetFiltered(TaskStatus? statusFilter)
+        {
+            IEnumerable<TaskItem> q = _tasks;
+            if (statusFilter != null)
+                q = q.Where(t => t.Status == statusFilter);
+            return q;
+        }
+    }
+}
